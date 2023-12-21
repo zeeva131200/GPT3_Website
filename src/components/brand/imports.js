b7ddac4915cb061394ebfd3b9ebdcbae898abf46
zeeva131200 <zeeva06.2@gmail.com>
2023-12-21 16:05:27 +0800

    first commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import googl from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+  googl,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+};
